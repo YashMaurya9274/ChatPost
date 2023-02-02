e136d4012b81950731b4bd6eedc42af0245b37e5
YashMaurya9274 <yash.maurya537468@gmail.com>
2023-02-03 02:08:15 +0530

    like issue resolved

--- src/components/PostComponent.tsx
+++ src/components/PostComponent.tsx
@@ -66,11 +66,13 @@ const PostComponent = ({post, fromUserProfileScreen}: Props) => {
 
   const getUserLikedIndex = () => {
     let result = -1;
-    postLikes?.map(postLike => {
-      if (postLike._ref === user.uid) {
-        result = postLikes.indexOf(postLike);
-      }
-    });
+    if (postLikes?.length! > 0) {
+      postLikes?.map(postLike => {
+        if (postLike._ref === user.uid) {
+          result = postLikes.indexOf(postLike);
+        }
+      });
+    }
 
     return result;
   };
@@ -121,7 +123,7 @@ const PostComponent = ({post, fromUserProfileScreen}: Props) => {
   };
 
   const likePost = async () => {
-    let tempLikes: LikeUser[] = postLikes!;
+    let tempLikes: LikeUser[] = postLikes || [];
 
     if (checkUserLiked()) {
       tempLikes.splice(getUserLikedIndex(), 1);
